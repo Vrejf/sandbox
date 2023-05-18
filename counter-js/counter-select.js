1659df6490fae36e6126161bc8430282da225798
Vrejf <jens.harvard@gmail.com>
2023-05-18 10:34:10 +0000

    v2

--- counter-js/counter-select.js
+++ counter-js/counter-select.js
@@ -1,47 +1,134 @@
 // Create an object to store the data-countername values
-let counterNames = {};
 
-// Find and select all elements with the attribute data-countername
-let elements = document.querySelectorAll("[data-countername]");
+const apiUrl = "https://utils-api.vercel.app/api/count/"
 
-// Loop through the elements
-for (let element of elements) {
-    // Get the value of data-countername attribute
-    let counterName = element.getAttribute("data-countername");
-
-    // Check if the value exists as a key in the object
-    if (counterNames.hasOwnProperty(counterName)) {
-        // Increment the value by one
-        counterNames[counterName]++;
+function calcPercent(current, target) {
+    if (current >= target) { return 100; }
+    return Math.round((current / target) * 100);
+}
+let counterNames = {
+    "test01": 123,
+};
+function getCount(name) {
+    if (counterNames.hasOwnProperty(name)) {
+        currentValueElement.textContent = counterNames[name]
+        return counterNames[name]
     } else {
-        // Add it to the object with value 0
-        counterNames[counterName] = 0;
-    }
-
-    // Find all elements with the class-name counter-current-value within the element
-    let currentValues = element.querySelectorAll(".counter-current-value");
-
-    // Loop through the current values
-    for (let currentValue of currentValues) {
-        // Console.log the text content of counter-current-value
-        console.log(currentValue.textContent);
-    }
-
-    // Find all elements with the class name counter-target-value within the element
-    let targetValues = element.querySelectorAll(".counter-target-value");
-
-    // Loop through the target values
-    for (let targetValue of targetValues) {
-        // Console.log the text content of counter-target-value
-        console.log(targetValue.textContent);
-    }
-
-    // Find all elements with the class name counterbar_limiter within the element
-    let limiters = element.querySelectorAll(".counterbar_limiter");
-
-    // Loop through the limiters
-    for (let limiter of limiters) {
-        // Console.log the width of counterbar_limiter
-        console.log(limiter.style.width);
+        console.log("fetching")
+        const url = new URL(name, apiUrl)
+        return fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    console.log(response.json())
+                    console.log(response.statusText)
+                }
+                return response.json()
+            })
+            .then(data => {
+                if (Number.isInteger(data.count)) {
+                    counterNames[name] = data.count;
+                    return data.count
+                } else {
+                    console.log("Count not Int")
+                    return 0
+                }
+            }).catch(e => {
+                console.log("Error ", e)
+            })
     }
 }
+
+
+// Find and select all elements with the attribute data-countername
+let counterElements = document.querySelectorAll("[data-countername]");
+// Loop through the elements
+for (let element of counterElements) {
+
+    let counterName = element.getAttribute("data-countername");
+
+    let targetValue = 999;
+    const localString = "sv-SE";
+    const hiddenStyle = `display: inline-block; transition: opacity 0.5s ease-in-out; opacity: 0`;
+
+    console.log(counterNames)
+
+    // Find all elements with the class-name counter-current-value within the element
+    let currentValueElements = element.querySelectorAll(".counter-current-value");
+
+    // Loop through the current values
+    for (let currentValueElement of currentValueElements) {
+
+        // Check if the countername exists
+        if (counterNames.hasOwnProperty(counterName)) {
+            currentValueElement.textContent = counterNames[counterName]
+        } else {
+            console.log("name not in list - currentvalueelement, getting it...")
+
+            getCount(counterName).then(currentValue => {
+                // DO stuff after the fetch is done loading
+                console.log("Current value: ", currentValue);
+                counterNames[counterName] = currentValue;
+                currentValueElement.textContent = counterNames[counterName]
+
+                // Find all counterbar-limiter
+                let limiters = element.querySelectorAll(".counterbar-limiter");
+                for (let limiter of limiters) {
+                    // Set the width of the limiter based on the current value
+                    limiter.style.width = calcPercent(currentValue, targetValue) + "%";
+                    console.log("Limiter percent", limiter.style.width);
+                }
+
+            });
+        }
+
+    }
+
+    // Find all counter-target-value
+    let targetValuesElements = element.querySelectorAll(".counter-target-value");
+    for (let targetValueElement of targetValuesElements) {
+
+        console.log("", targetValueElement.textContent);
+
+    }
+
+    // Find all counterbar-limiter
+    let limiters = element.querySelectorAll(".counterbar-limiter");
+    for (let limiter of limiters) {
+
+        // Check if the countername exists
+        if (counterNames.hasOwnProperty(counterName)) {
+            console.log("Limiter, name in list")
+            limiter.style.width = calcPercent(counterNames[counterName], targetValue) + "%";
+            console.log("Limiter percent", limiter.style.width);
+        } else {
+            // Add it to the object with value 0
+            console.log("name not in list - Limiter, getting it...")
+            getCount(counterName).then(currentValue => {
+                // DO stuff after the fetch is done loading
+                console.log("Current value: ", currentValue);
+                counterNames[counterName] = currentValue;
+                // Find all counterbar-limiter
+                let limiters = element.querySelectorAll(".counterbar-limiter");
+                for (let limiter of limiters) {
+                    // Set the width of the limiter based on the current value
+                    limiter.style.width = calcPercent(currentValue, targetValue) + "%";
+                    console.log("Limiter percent", limiter.style.width);
+                }
+
+            });
+        }
+
+    }
+    if (element.tagName === 'FORM') {
+        // It is a form element
+        console.log('Found a form element:', element);
+
+        // Perform further operations specific to form elements
+        // ...
+    } else {
+        // It is not a form element
+        console.log('Not a form element:', element);
+    }
+}
+
+
