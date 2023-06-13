c1a504abb6ea5de6fd05e1bf2b966be98dcc861b
Vrejf <jens.harvard@gmail.com>
2023-06-13 15:26:15 +0200

    new prep object

--- agera-sync/agera-sync.0.0.3.js
+++ agera-sync/agera-sync.0.0.3.js
@@ -1,35 +1,119 @@
-// agera-sync.0.0.2.js 23-06-10 19.00
+// agera-sync.0.0.3.js 23-06-13 15.00
 // Data attributes: data-crm, data-redirect-utm, data-counter-update
 function ageraSync(form) {
-    const constants = {
+    const params = {
         thisUrl: new URL(window.location.href),
         wfFormUrl: "https://webflow.com/api/v1/form/",
-        counterUrl: "https://utils-api.vercel.app/api/count/",
         wfSiteId: document.querySelector("html").dataset.wfSite,
-        //niceUtms: [],
-        niceUtms: Array.from(new URLSearchParams(window.location.search), ([key, value]) => `${key}: ${value}`)
-
-    }
-    const utmSource = constants.thisUrl.searchParams.get("utm_source") || constants.thisUrl.searchParams.get("source");
-    const submitButton = form.querySelector('input[type="submit"]') || undefined;
-    const submitText = (submitButton && submitButton.value) || "";
-
-    const options = {
+        wfUrl: new URL(document.querySelector("html").dataset.wfSite, "https://webflow.com/api/v1/form/"),
+        counterUrl: "https://utils-api-git-experimental-vrejf.vercel.app/api/counter/",
+        niceUtms: Array.from(new URLSearchParams(window.location.search), ([key, value]) => `${key}: ${value}`),
         redirect: form.getAttribute("redirect") || false,
         endpoint: form.getAttribute("action") || undefined,
-        addUtm: Boolean(form.dataset.redirectUtm) || true
+        addUtm: Boolean(form.dataset.redirectUtm) || true,
+        utmSource: new URLSearchParams(window.location.search).get("utm_source") || new URLSearchParams(window.location.search).get("source"),
+        submitButton: form.querySelector('input[type="submit"]') || undefined,
+        submitText: (this.submitButton && this.submitButton.value) || "",
     }
+
     const prepData = {
-        formData: new FormData(form),
-        an() {
-            console.log("preppar data")
-            return "hejs"
+        data: { url: new URL(params.endpoint).toString(), method: "POST", headers: { "Content-Type": "application/json" } },
+
+        webFlow(form) {
+            console.log("preppar data för webflow")
+            const formData = new FormData(form).append("UTM", params.niceUtms)
+            const uriBody = new URLSearchParams({
+                name: form.getAttribute("name"),
+                source: window.location.href,
+            });
+
+            for (const pair of formData.entries()) {
+                const fieldName = `fields[${pair[0]}]`;
+                uriBody.append(fieldName, pair[1]);
+            }
+            return this.data = {
+                url: params.wfUrl.href,
+                body: uriBody.toString(),
+                headers: { "Content-Type": "application/x-www-form-urlencoded" }
+            };
         },
-    }
+        mailChimp(form) {
+            console.log("preppar data för mailchimp")
+            const formData = new FormData(form);
+            const uriBody = new URLSearchParams(formData)
+            uriBody.append("UTM", params.niceUtms)
+            return this.data = {
+                url: params.endpoint.replace('post?', 'post-json?') + '&c=?',
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                dataType: "jsonp",
+                body: uriBody.toString()
+            };
+        },
+        zapier(form) {
+            return this.data = {
+                body: JSON.stringify({
+                    [form.getAttribute("name")]: Object.fromEntries(new FormData(form)),
+                    "source": params.thisUrl.toString(),
+                    "time": new Date().toISOString(),
+                    "UTM": params.niceUtms,
+                })
+            }
+        },
+        counter(counterName) {
+            return this.data = {
+                url: new URL(counterName, params.counterUrl).toString(),
+                body: JSON.stringify({
+                    name: "default",
+                    site: params.wfSiteId,
+                })
+            }
+        },
+        actionNetwork(form) {
+            const formData = new FormData(form);
+            return this.data = {
+                body:
+                    JSON.stringify({
+                        person: {
+                            given_name: formData.get("first"),
+                            family_name: formData.get("last"),
+                            email_addresses: [
+                                {
+                                    address: formData.get("email_address")
+                                }
+                            ],
+                            phone_numbers: [
+                                {
+                                    number: formData.get("phone_number") || ""
+
+                                }
+                            ],
+                            postal_addresses: [
+                                {
+                                    postal_code: formData.get("postal_code") || "",
+                                    address_lines: formData.get("address") || "",
+                                    region: formData.get("region") || "",
+                                    country: formData.get("country") || ""
+                                }
+                            ]
+                        },
+                        "add_tags": [
+                            formData.getAll("tags") || ""
+                        ],
+                        "action_network:referrer_data": {
+                            source: params.utmSource ? params.utmSource.toString() : "",
+                            website: params.thisUrl.hostname + params.thisUrl.pathname
+                        }
+                    }),
+            };
+        }
+    };
+
     function prepAnData(form) {
         const formData = new FormData(form);
         const data = {
-            url: options.endpoint,
+            url: params.endpoint,
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body:
@@ -61,19 +145,18 @@ function ageraSync(form) {
                         formData.getAll("tags") || ""
                     ],
                     "action_network:referrer_data": {
-                        source: utmSource ? utmSource.toString() : "",
-                        website: constants.thisUrl.hostname + constants.thisUrl.pathname
+                        source: params.utmSource ? params.utmSource.toString() : "",
+                        website: params.thisUrl.hostname + params.thisUrl.pathname
                     }
                 }),
         };
         return data;
     };
 
-
     function prepWfData(form) {
         const formData = new FormData(form);
-        formData.append("UTM", constants.niceUtms)
-        const wfUrl = new URL(constants.wfSiteId, constants.wfFormUrl)
+        formData.append("UTM", params.niceUtms)
+        const wfUrl = new URL(params.wfSiteId, params.wfFormUrl)
         const uriBody = new URLSearchParams();
         uriBody.append('name', form.getAttribute("name"));
         uriBody.append('source', window.location.href);
@@ -97,9 +180,9 @@ function ageraSync(form) {
     function prepMailChimpData(form) {
         const formData = new FormData(form);
         const uriBody = new URLSearchParams(formData);
-        uriBody.append("UTM", constants.niceUtms);
+        uriBody.append("UTM", params.niceUtms);
         const data = {
-            url: options.endpoint.replace('post?', 'post-json?') + '&c=?',
+            url: params.endpoint.replace('post?', 'post-json?') + '&c=?',
             method: "POST",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
@@ -113,30 +196,29 @@ function ageraSync(form) {
     function prepZapierData(form) {
         const formData = new FormData(form);
         const data = {
-            url: new URL(options.endpoint).toString(),
+            url: new URL(params.endpoint).toString(),
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
                 [form.getAttribute("name")]: Object.fromEntries(formData),
-                "source": constants.thisUrl.toString(),
+                "source": params.thisUrl.toString(),
                 "time": new Date().toISOString(),
-                "UTM": constants.niceUtms,
+                "UTM": params.niceUtms,
             })
         };
         return data;
     }
 
-
     function prepCounterData(counterName) {
         const data = {
-            url: new URL(counterName, constants.counterUrl).toString(),
+            url: new URL(counterName, params.counterUrl).toString(),
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
                 name: "default",
-                site: constants.wfSiteId
+                site: params.wfSiteId
             })
         }
         return data;
@@ -153,17 +235,17 @@ function ageraSync(form) {
         } else {
             doneElement.style.display = 'none';
             failElement.style.display = 'block';
-            submitButton.value = submitText;
+            params.submitButton.value = params.submitText;
         }
 
     }
 
     function redirect() {
-        if (options.redirect) {
+        if (params.redirect) {
             console.log("Redirecting...")
-            if (options.addUtm) {
+            if (params.addUtm) {
 
-                const redirectUrl = new URL(options.redirect, constants.thisUrl);
+                const redirectUrl = new URL(params.redirect, params.thisUrl);
                 const utmParams = new URLSearchParams(window.location.search);
 
                 redirectUrl.searchParams.forEach((value, param) => {
@@ -176,7 +258,7 @@ function ageraSync(form) {
 
                 window.location.href = updatedRedirectUrl;
             } else {
-                window.location.href = options.redirect;
+                window.location.href = params.redirect;
             }
         } else {
             console.log("No redirect defined")
@@ -238,7 +320,7 @@ function ageraSync(form) {
         form.addEventListener("submit", async function (event) {
             event.preventDefault();
             event.stopPropagation();
-            submitButton.value = submitButton.dataset.wait || "Sending...";
+            params.submitButton.value = params.submitButton.dataset.wait || "Sending...";
 
             // list with all request DATA:
             let requestList = []
@@ -246,15 +328,15 @@ function ageraSync(form) {
             const counterUpdate = form.dataset.counterUpdate;
 
             if (crm === "webflow") {
-                requestList.push(prepWfData(form));
+                requestList.push(prepData.webFlow(form));
             }
-            if (crm === "actionnetwork" && options.endpoint) {
+            if (crm === "actionnetwork" && params.endpoint) {
                 requestList.push(prepAnData(form));
             }
-            if (crm === "mailchimp" && options.endpoint) {
+            if (crm === "mailchimp" && params.endpoint) {
                 requestList.push(prepMailChimpData(form));
             }
-            if (crm === "zapier" && options.endpoint) {
+            if (crm === "zapier" && params.endpoint) {
                 requestList.push(prepZapierData(form));
             }
             if (counterUpdate && counterUpdate !== "default") {
